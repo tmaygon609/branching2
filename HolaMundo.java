a45354d8c9e530105585b6498f1ba7a3e965b2f4
Tibu <tibumayo88@gmail.com>
2023-11-19 18:36:55 +0100

    Versión inicial del fichero HolaMundo.java sin colores

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/**
+ * Muestra la frase Hola Mundo
+ */
+public class HolaMundo {
+    public static void main (String[] args) {
+        System.out.println("¡Hola mundo!");
+        System.out.println("Aquí estamos haciendo pruebas en Git.");
+    }
+}
