d3e100ef93bf01e896b68a8dc77ae51a3b7edeb4
Tibu <tibumayo88@gmail.com>
2023-11-19 18:39:44 +0100

    Versión del fichero HolaMundo.java con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@
  */
 public class HolaMundo {
     public static void main (String[] args) {
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("¡Hola mundo!");
         System.out.println("Aquí estamos haciendo pruebas en Git.");
     }
