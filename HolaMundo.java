b5e787358f9e0435ae4c243903b16085b8cb5a02
Tibu <tibumayo88@gmail.com>
2023-11-19 18:48:11 +0100

    Se ha cambiado el codigo del color rojo por el correcto

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@
  */
 public class HolaMundo {
     public static void main (String[] args) {
-        String rojo = "\033[32m";
+        String rojo = "\033[31m";
         String azul = "\033[34m";
         System.out.println(rojo + "¡Hola mundo!");
         System.out.println(azul + "Aquí estamos haciendo pruebas en Git.");
