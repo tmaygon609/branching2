0a1ccefbefc400b709aef5b7954210fa8df044e9
Tibu <tibumayo88@gmail.com>
2023-11-19 18:43:40 +0100

    Saludo en colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
     public static void main (String[] args) {
         String rojo = "\033[32m";
         String azul = "\033[34m";
-        System.out.println("¡Hola mundo!");
-        System.out.println("Aquí estamos haciendo pruebas en Git.");
+        System.out.println(rojo + "¡Hola mundo!");
+        System.out.println(azul + "Aquí estamos haciendo pruebas en Git.");
     }
 }
